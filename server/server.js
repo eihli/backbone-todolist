eeb41942dadc4a7dddc6c8923acfd24f618e8ae7
Eric Ihli <ericihli@gmail.com>
2015-06-04 13:41:38 -0700

    Pass hostname into app.listen so console logs correct address.

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, 'localhost', function () {
 
   var host = server.address().address;
   var port = server.address().port;
